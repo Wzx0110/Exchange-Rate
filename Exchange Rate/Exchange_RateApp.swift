3ab990d1695441a9ab780a893e4be7ecbbc5ba2d
ZiXiang <wzxiang9301@gmail.com>
2024-10-19 01:20:10 +0800

    Initial Commit

---
+++ Exchange Rate/Exchange_RateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exchange_RateApp.swift
+//  Exchange Rate
+//
+//  Created by 翁子翔 on 2024/10/19.
+//
+
+import SwiftUI
+
+@main
+struct Exchange_RateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Exchange RateTests/Exchange_RateTests.swift
@@ -0,0 +1,17 @@
+//
+//  Exchange_RateTests.swift
+//  Exchange RateTests
+//
+//  Created by 翁子翔 on 2024/10/19.
+//
+
+import Testing
+@testable import Exchange_Rate
+
+struct Exchange_RateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
